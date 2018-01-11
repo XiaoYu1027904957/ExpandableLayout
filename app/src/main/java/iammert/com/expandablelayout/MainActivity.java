715f4beb964eb8c8e1a26ab9ce7f55fa3dcf0a1c
xiaoyu <1027904957@qq.com>
2018-01-11 18:22:48 +0800

    commit

---
+++ app/src/main/java/iammert/com/expandablelayout/MainActivity.java
@@ -0,0 +1,103 @@
+package iammert.com.expandablelayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import iammert.com.expandablelib.ExpandCollapseListener;
+import iammert.com.expandablelib.ExpandableLayout;
+import iammert.com.expandablelib.Section;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] parents = new String[]{"Fruits",
+            "Nice Fruits", "Cool Fruits",
+            "Perfect Fruits", "Frozen Fruits",
+            "Warm Fruits"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        EditText editText = (EditText) findViewById(R.id.edittext);
+        ExpandableLayout sectionLinearLayout = (ExpandableLayout) findViewById(R.id.el);
+
+        sectionLinearLayout.setRenderer(new ExpandableLayout.Renderer<FruitCategory, Fruit>() {
+            @Override
+            public void renderParent(View view, FruitCategory model, boolean isExpanded, int parentPosition) {
+                ((TextView) view.findViewById(R.id.tvParent)).setText(model.name);
+                view.findViewById(R.id.arrow).setBackgroundResource(isExpanded ? R.drawable.arrow_up : R.drawable.arrow_down);
+            }
+
+            @Override
+            public void renderChild(View view, Fruit model, int parentPosition, int childPosition) {
+                ((TextView) view.findViewById(R.id.tvChild)).setText(model.name);
+            }
+        });
+
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+        sectionLinearLayout.addSection(getSection());
+
+        sectionLinearLayout.setExpandListener((ExpandCollapseListener.ExpandListener<FruitCategory>) (parentIndex, parent, view) -> {
+
+        });
+
+        sectionLinearLayout.setCollapseListener((ExpandCollapseListener.CollapseListener<FruitCategory>) (parentIndex, parent, view) -> {
+
+        });
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                sectionLinearLayout.filterChildren(obj -> ((Fruit) obj).name.toLowerCase().contains(s.toString().toLowerCase()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public Section<FruitCategory, Fruit> getSection() {
+        Section<FruitCategory, Fruit> section = new Section<>();
+        FruitCategory fruitCategory = new FruitCategory(parents[(int) (Math.random() * parents.length)]);
+        Fruit fruit1 = new Fruit("Apple");
+        Fruit fruit2 = new Fruit("Orange");
+        Fruit fruit3 = new Fruit("Banana");
+        Fruit fruit4 = new Fruit("Grape");
+        Fruit fruit5 = new Fruit("Strawberry");
+        Fruit fruit6 = new Fruit("Cherry");
+
+        section.parent = fruitCategory;
+        section.children.add(fruit1);
+        section.children.add(fruit2);
+        section.children.add(fruit3);
+        section.children.add(fruit4);
+        section.children.add(fruit5);
+        section.children.add(fruit6);
+        section.expanded = true;
+        return section;
+    }
+}
---
+++ expandablelib/src/main/java/iammert/com/expandablelib/ExpandableLayout.java
@@ -0,0 +1,281 @@
+package iammert.com.expandablelib;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mertsimsek on 28/07/2017.
+ */
+
+public class ExpandableLayout extends LinearLayout {
+
+    public interface Renderer<P, C> {
+        void renderParent(View view, P model, boolean isExpanded, int parentPosition);
+
+        void renderChild(View view, C model, int parentPosition, int childPosition);
+    }
+
+    private static final int NO_RES = 0;
+
+    private static final int NO_INDEX = -1;
+
+    private static final Operation DEFAULT_FILTER = new Operation() {
+        @Override
+        public boolean apply(Object obj) {
+            return true;
+        }
+    };
+
+    private LayoutInflater layoutInflater;
+
+    @LayoutRes
+    private int parentLayout;
+
+    @LayoutRes
+    private int childLayout;
+
+    private Renderer renderer;
+
+    private List<Section> sections;
+
+    private ExpandCollapseListener.ExpandListener expandListener;
+
+    private ExpandCollapseListener.CollapseListener collapseListener;
+
+    private Operation currentFilter = DEFAULT_FILTER;
+
+    public ExpandableLayout(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public ExpandableLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public ExpandableLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ExpandableLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attributeSet) {
+        setOrientation(VERTICAL);
+        sections = new ArrayList<>();
+        TypedArray typedArray = null;
+        try {
+            typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.ExpandableLayout);
+            parentLayout = typedArray.getResourceId(R.styleable.ExpandableLayout_parentLayout, NO_RES);
+            childLayout = typedArray.getResourceId(R.styleable.ExpandableLayout_childLayout, NO_RES);
+            layoutInflater = LayoutInflater.from(context);
+        } finally {
+            if (typedArray != null) {
+                typedArray.recycle();
+            }
+        }
+    }
+
+    public <P> void setExpandListener(ExpandCollapseListener.ExpandListener<P> expandListener) {
+        this.expandListener = expandListener;
+    }
+
+    public <P> void setCollapseListener(ExpandCollapseListener.CollapseListener<P> collapseListener) {
+        this.collapseListener = collapseListener;
+    }
+
+    public void setRenderer(@NonNull Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public void addSection(@NonNull Section section) {
+        sections.add(section);
+        notifySectionAdded(section);
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public <P, C> void addChild(P parent, C child) {
+        int parentIndex = NO_INDEX;
+        for (int i = 0; i < sections.size(); i++) {
+            if (sections.get(i).parent.equals(parent)) {
+                if (!sections.get(i).children.contains(child)) {
+                    sections.get(i).children.add(child);
+                }
+                parentIndex = i;
+            }
+        }
+        if (parentIndex != NO_INDEX) {
+            notifyItemAdded(parentIndex, child);
+            if (sections.get(parentIndex).expanded) {
+                expand(parent);
+            }
+        }
+    }
+
+    public <P, C> void addChildren(P parent, List<C> children) {
+        int parentIndex = NO_INDEX;
+        for (int i = 0; i < sections.size(); i++) {
+            if (sections.get(i).parent.equals(parent)) {
+                if (!sections.get(i).children.containsAll(children)) {
+                    sections.get(i).children.addAll(children);
+                }
+                parentIndex = i;
+            }
+        }
+        if (parentIndex != NO_INDEX) {
+            notifyItemAdded(parentIndex, children);
+            if (sections.get(parentIndex).expanded) {
+                expand(parent);
+            }
+        }
+    }
+
+    public void filterParent(@NonNull Operation op) {
+        for (Section section : getSections()) {
+            final Object parent = section.parent;
+            final boolean contains = op.apply(parent);
+            getChildAt(sections.indexOf(section)).setVisibility(contains ? View.VISIBLE : View.GONE);
+        }
+    }
+
+
+    public void filterChildren(@NonNull Operation op) {
+        currentFilter = op;
+        for (Section section : getSections()) {
+            final List children = section.children;
+            boolean keepParentVisible = false;
+            final ViewGroup childrenViews = (ViewGroup) getChildAt(sections.indexOf(section));
+            for (int i = 0; i < children.size(); i++) {
+                final Object child = children.get(i);
+                final boolean contains = op.apply(child);
+                childrenViews.getChildAt(children.indexOf(child) + 1).setVisibility(contains ? View.VISIBLE : View.GONE);
+                if (!keepParentVisible && contains) {
+                    keepParentVisible = true;
+                }
+
+            }
+            childrenViews.setVisibility(keepParentVisible ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    private <C> void notifyItemAdded(int parentIndex, C child) {
+        if (renderer == null) {
+            return;
+        }
+        ViewGroup parentView = (ViewGroup) getChildAt(parentIndex);
+        View childView = layoutInflater.inflate(childLayout, null);
+        renderer.renderChild(childView, child, parentIndex, sections.get(parentIndex).children.size() - 1);
+        parentView.addView(childView);
+    }
+
+    private <C> void notifyItemAdded(int parentIndex, List<C> children) {
+        if (renderer == null) {
+            return;
+        }
+        ViewGroup parentView = (ViewGroup) getChildAt(parentIndex);
+        for (int i = 0; i < children.size(); i++) {
+            View childView = layoutInflater.inflate(childLayout, null);
+            renderer.renderChild(childView, children.get(i), parentIndex, i);
+            parentView.addView(childView);
+        }
+    }
+
+    private void notifySectionAdded(final Section section) {
+        if (renderer == null)
+            return;
+
+        LinearLayout sectionLayout = new LinearLayout(getContext());
+        sectionLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        sectionLayout.setOrientation(LinearLayout.VERTICAL);
+
+        View parentView = layoutInflater.inflate(parentLayout, null);
+        parentView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (section.expanded) {
+                    collapse(section.parent);
+                } else {
+                    expand(section.parent);
+                }
+            }
+        });
+        renderer.renderParent(parentView, section.parent, section.expanded, sections.size() - 1);
+        sectionLayout.addView(parentView);
+
+        if (section.expanded) {
+            for (int i = 0; i < section.children.size(); i++) {
+                Object child = section.children.get(i);
+                View childView = layoutInflater.inflate(childLayout, null);
+                renderer.renderChild(childView, child, sections.size() - 1, i);
+                sectionLayout.addView(childView);
+            }
+        }
+        addView(sectionLayout);
+    }
+
+    public void notifyParentChanged(int position) {
+        if (position > getChildCount() - 1) {
+            return;
+        }
+        ViewGroup viewGroup = (ViewGroup) getChildAt(position);
+        if (viewGroup != null && viewGroup.getChildCount() > 0) {
+            View parentView = viewGroup.getChildAt(0);
+            renderer.renderParent(parentView, sections.get(position).parent, sections.get(position).expanded, position);
+        }
+    }
+
+    private <P> void expand(@NonNull P parent) {
+        for (int i = 0; i < sections.size(); i++) {
+            if (parent.equals(sections.get(i).parent)) {
+                ViewGroup sectionView = ((ViewGroup) getChildAt(i));
+                for (int j = 1; j < sectionView.getChildCount(); j++) {
+                    final View childView = sectionView.getChildAt(j);
+                    final Object childType = sections.get(i).children.get(j - 1);
+                    childView.setVisibility(currentFilter.apply(childType) ? View.VISIBLE : View.GONE);
+                }
+                sections.get(i).expanded = true;
+                if (expandListener != null)
+                    expandListener.onExpanded(i, sections.get(i).parent, sectionView.getChildAt(0));
+                break;
+            }
+        }
+    }
+
+    private <P> void collapse(@NonNull P parent) {
+        for (int i = 0; i < sections.size(); i++) {
+            if (parent.equals(sections.get(i).parent)) {
+                ViewGroup sectionView = ((ViewGroup) getChildAt(i));
+                sections.get(i).expanded = false;
+
+                for (int j = 1; j < sectionView.getChildCount(); j++) {
+                    sectionView.getChildAt(j).setVisibility(View.GONE);
+                }
+
+                if (collapseListener != null)
+                    collapseListener.onCollapsed(i, sections.get(i).parent, sectionView.getChildAt(0));
+                break;
+            }
+        }
+    }
+}
